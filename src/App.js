40e74b1e7d6839b5ffb2917f7c2127914255853b
Aarya Chopkar <aaryachopkar@gmail.com>
2022-06-12 15:09:51 +0530

    First React Project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
+// import logo from './logo.svg';
 import './App.css';
-function App() {
+import Main from "./components/Main"
+import Navbar from "./components/Navbar"
+
+// function Navbar(){
+//   return (
+//     <>
+//         <nav>
+//         <img src={logo} className="nav-img" alt="" />
+//         <h3 className="nav-head">ReactFacts</h3>
+//         <h4 className="title">React Project 1</h4>
+//         </nav>
+//         </>
+//   )
+// }
+
+// function Main(){
+//   return (
+//       <>
+//       <div className="container">
+//           <h1>Fact About React</h1>
+//           <ul>
+//               <li>Was first released in 2013</li>
+//               <li>Was originally created by Jordan Walke</li>
+//               <li>Has well over 100K stars on GitHub</li>
+//               <li>Is maintained by Facebook</li>
+//               <li>Powers thousands of enterprise apps, including mobile apps</li>
+//           </ul>
+//       </div>
+//       </>
+//   )
+// }
+
+function App(){
   return (
     <>
-    <div className='container'>
-      <h1>My Awesome Website in React</h1>
-      <h3>Reasons I like React</h3>
-      <ol>
-        <li>It's composable</li>
-        <li>It's declarative</li>
-        <li>It's a hireable skill</li>
-        <li>It's actively maintained by skillfull people</li>
-      </ol>
-    </div>
+    <Navbar/>
+    <Main/>
     </>
   );
 }
