8fa04a8b0fe0f6c1dece02bbcd7ca299a21a4b6d
Aarya Chopkar <aaryachopkar@gmail.com>
2022-07-14 19:47:19 +0530

    Props

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,21 @@ import React from "react";
 import logo from '../logo.svg';
 
 
-function Navbar(){
+function Navbar(props){
     return (
       <>
-          <nav>
+          <nav className={props.darkMode ? "dark": ""}>
           <img src={logo} className="nav-img" alt="" />
           <h3 className="nav-head">ReactFacts</h3>
           <h4 className="title">React Project 1</h4>
+          <div className="toggler">
+              <p className="toggler--light">Light</p>
+                <div className="toggler--slider"
+                    onClick={props.toggleDarkMode}>
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
           </nav>
           </>
     )
